99208cb886c17fb842a4567cfa163c0a41bae06d
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2024-05-13 19:27:09 +0545

    last

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const cors = require("cors");
 const session = require("express-session");
 const parser = require("body-parser");
 const authRouter = require("./router/Auth.router");
-const messageRouter=require("./router/Message.router");
-const userRouter= require("./router/Users.router")
+const messageRouter = require("./router/Message.router");
+const userRouter = require("./router/Users.router");
 const { app, server } = require("./socket/socket");
 connectToDb();
 app.use(
@@ -16,14 +16,23 @@ app.use(
     credentials: true,
   })
 );
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://6642145f046ec10398e24193--fascinating-belekoy-23f79b.netlify.app/"
+  );
+  next();
+});
 app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 app.use(express.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(cookieParser());
 //
 app.use("/api/", authRouter.router);
-app.use("/api",messageRouter.router);
-app.use("/api",userRouter.router);
+app.use("/api", messageRouter.router);
+app.use("/api", userRouter.router);
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`App is Running ${PORT}`);
